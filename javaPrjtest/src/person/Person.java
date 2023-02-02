7179b9fce56082ad5efede1759d7f04c67fec3e9
DESKTOP-9AL2P16\user2 <yoonssong123@naver.com>
2023-02-02 19:24:51 +0900

    연습

---
+++ javaPrjtest/src/person/Person.java
@@ -0,0 +1,19 @@
+package person;
+
+public class Person {
+	public String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [name=" + name + "]";
+	}
+	
+}
